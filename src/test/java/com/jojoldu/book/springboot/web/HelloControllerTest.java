7951b8ac17644a03d9734aa6bd716f4c58b69868
hkleev2 <hkleev2@gmail.com>
2020-10-28 20:41:30 +0900

    테스트 실패 수정
    1. HelloContollerTest 문제
    
    @WebMvcTest 어노테이션이 붙으면 테스트 로딩하면서 @Controller 어노테이션이 붙은 클래스들을 싹 다 로딩시킨다고하네요
    @Service 는 로딩을 안시키구요
    PostsApiController에 @Controller 가 있어서 얘를 로딩하는데 내부 의존성인 PostsService를 로딩하려고 하는데 얘는 @Service로 선언돼있는 컴포넌트라서 로딩이 안됐던거였어용
    해결방법은 @WebMvcTest 어노테이션에 로딩할 컨트롤러를 지정해버리면 된다고 하네용 (소스 변경사항 참고)
    
    2. PostsApiControllerTest
    RestTemplate이 아니라 TestRestTemplate 이었어용
    그래서 @Autowired가 안됐던거같아용 클래스타입이 달라서

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킴
 // 여기서는 SpringRunner라는 스프링 실행자를 사용
 // 즉, 스프링부트 테스트와 JUnit 사이에 연결자 역할
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
 
     @Autowired
